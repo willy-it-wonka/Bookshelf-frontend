eecc45257c55484c1d0e23d6ae571cf7197c8c6f
Willy <willy.it.wonka@outlook.com>
2024-03-21 22:54:03 +0100

    Create user - post to back-end

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,6 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UserService {}
+export class UserService {
+  private registerUrl = 'http://localhost:8080/api/register';
+
+  constructor(private httpClient: HttpClient) {}
+
+  /*HTTP POST to register a new user.*/
+  createUser(user: User): Observable<Object> {
+    return this.httpClient.post(`${this.registerUrl}`, user);
+  }
+}
