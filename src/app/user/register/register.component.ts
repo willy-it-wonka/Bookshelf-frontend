74dc4b892aba5d5e1a08ffd94e859ea431cbc8c6
Willy <willy.it.wonka@outlook.com>
2024-04-28 23:07:59 +0200

    Add reCAPTCHA to registration

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -2,16 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../user';
 import { UserService } from '../user.service';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgxCaptchaModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
   user: User = new User();
+
+  /*
+  Usage reCAPTCHA:
+  1. Login to your goole account.
+  2. Go to google.com/recaptcha/admin/create and configure it, click send, and you will get siteKey. */
+  siteKey: string = 'YOUR_siteKey';
+  isCaptchaResolved: boolean = false;
 
   constructor(private userService: UserService) {}
 
@@ -29,5 +37,9 @@ export class RegisterComponent implements OnInit {
         console.log(error), alert(error.error);
       },
     });
+  }
+
+  onCaptchaResolved(resolved: any) {
+    this.isCaptchaResolved = true;
   }
 }
