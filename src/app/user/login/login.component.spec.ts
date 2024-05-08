3aedae3e54d73c55dec99e0fd6d4f39a31842424
Willy <willy.it.wonka@outlook.com>
2024-05-08 20:51:52 +0200

    Test LoginComponent

--- src/app/user/login/login.component.spec.ts
+++ src/app/user/login/login.component.spec.ts
@@ -1,22 +1,61 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserService } from '../user.service';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+  let userData: { email: string; password: string };
 
   beforeEach(async () => {
+    userService = jasmine.createSpyObj('UserService', ['login']);
+
     await TestBed.configureTestingModule({
-      imports: [LoginComponent, HttpClientTestingModule],
+      imports: [LoginComponent],
+      providers: [{ provide: UserService, useValue: userService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    userData = { email: 'test@example.com', password: '123' };
+    component.email = userData.email;
+    component.password = userData.password;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call signIn() when onSubmit() is called', () => {
+    spyOn(component, 'signIn'); // Mocking signIn().
+    component.onSubmit();
+    expect(component.signIn).toHaveBeenCalled();
+  });
+
+  it('should handle successful login and redirect to bookshelf', () => {
+    const response = { message: 'test_token', status: true };
+    userService.login.and.returnValue(of(response)); // Mocking login() from UserService
+    spyOn(component, 'goToBookshelf'); // Mocking goToBookshelf().
+
+    component.signIn();
+
+    expect(userService.login).toHaveBeenCalledWith(userData);
+    expect(localStorage.getItem('jwt')).toEqual('test_token');
+    expect(component.goToBookshelf).toHaveBeenCalled();
+  });
+
+  it('should handle unsuccessful login', () => {
+    const response = { message: 'error message', status: false };
+    userService.login.and.returnValue(of(response));
+    spyOn(window, 'alert');
+
+    component.signIn();
+
+    expect(userService.login).toHaveBeenCalledWith(userData);
+    expect(window.alert).toHaveBeenCalledWith('error message');
   });
 });
