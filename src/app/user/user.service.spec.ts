00939b430486afe4d76fe8125805ad4c1d897efa
Willy <willy.it.wonka@outlook.com>
2024-05-10 22:01:15 +0200

    Test UserService

--- src/app/user/user.service.spec.ts
+++ src/app/user/user.service.spec.ts
@@ -1,16 +1,94 @@
 import { TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { User } from './user';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
+  const baseUrl = 'http://localhost:8080/api';
+  const user: User = {
+    nick: 'user',
+    email: 'test@Example.com',
+    password: '123',
+  };
+  const id = '1';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({imports: [HttpClientTestingModule],});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
+    });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verify that no unmatched requests are outstanding.
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should post user data to register API and return user data', () => {
+    service.register(user).subscribe((response) => {
+      expect(response).toEqual(user);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/register`);
+    expect(req.request.method).toBe('POST');
+    req.flush(user);
+  });
+
+  it('should post credentials to login API and return token', () => {
+    const responseMock = { token: '12345', status: true };
+
+    service.login(user).subscribe((res) => {
+      expect(res).toEqual(responseMock);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/login`);
+    expect(req.request.method).toBe('POST');
+    req.flush(responseMock);
+  });
+
+  it('should call logout API and return a success message', () => {
+    const responseMock = 'Logged out.';
+
+    service.logout().subscribe((res) => {
+      expect(res).toBe(responseMock);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/logout`);
+    expect(req.request.method).toBe('GET');
+    req.flush(responseMock);
+  });
+
+  it('should ask about user status (enabled) and return it', () => {
+    const responseMock = false;
+
+    service.checkEnabled(id).subscribe((enabled) => {
+      expect(responseMock).toBeFalse();
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/enabled/${id}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(responseMock);
+  });
+
+  it('should send post request to trigger the sending a new confirmation email', () => {
+    const responseMock = 'New email sent.';
+
+    service.sendNewConfirmationEmail(id).subscribe((res) => {
+      expect(res).toBe(responseMock);
+    });
+
+    const req = httpMock.expectOne(`${baseUrl}/new-conf-email/${id}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(responseMock);
   });
 });
