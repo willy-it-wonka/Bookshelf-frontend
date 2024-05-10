9034e08c18adb3c0e1c5d5e8f24cc4c5cd78a2e7
Willy <willy.it.wonka@outlook.com>
2024-05-10 20:50:23 +0200

    Test ContactComponent

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContactComponent } from './contact.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxCaptchaModule } from 'ngx-captcha';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
@@ -7,7 +10,7 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContactComponent],
+      imports: [ContactComponent, ReactiveFormsModule, NgxCaptchaModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ContactComponent);
@@ -17,5 +20,31 @@ describe('ContactComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have the form initialized with empty fields', () => {
+    const formGroup = component.formGroup;
+    expect(formGroup.valid).toBeFalsy(); // Form shouldn't be valid initially.
+    expect(formGroup.controls['from_email'].value).toEqual('');
+    expect(formGroup.controls['from_name'].value).toEqual('');
+    expect(formGroup.controls['subject'].value).toEqual('');
+    expect(formGroup.controls['message'].value).toEqual('');
+  });
+
+  it('should call emailjs.send when sendEmail() is called', async () => {
+    const mockResponse: EmailJSResponseStatus = { status: 200, text: 'OK' }; // Mocking response from EmailJS.
+    spyOn(emailjs, 'send').and.returnValue(Promise.resolve(mockResponse));
+    spyOn(component.formGroup, 'reset');
+
+    await component.sendEmail();
+
+    expect(emailjs.send).toHaveBeenCalled();
+    expect(component.formGroup.reset).toHaveBeenCalled();
+  });
+
+  it('should set isCaptchaResolved to true when captcha is resolved', () => {
+    const resolvedValue = true;
+    component.onCaptchaResolved(resolvedValue);
+    expect(component.isCaptchaResolved).toBeTruthy();
   });
 });
