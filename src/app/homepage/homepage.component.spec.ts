39f8505228c7614087f4411826c2c6a6b4a26d26
Willy <willy.it.wonka@outlook.com>
2024-05-10 19:19:04 +0200

    Test HomepageComponent

--- src/app/homepage/homepage.component.spec.ts
+++ src/app/homepage/homepage.component.spec.ts
@@ -1,21 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomepageComponent } from './homepage.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
 describe('HomepageComponent', () => {
   let component: HomepageComponent;
   let fixture: ComponentFixture<HomepageComponent>;
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomepageComponent],
+      imports: [HomepageComponent, RouterTestingModule.withRoutes([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomepageComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to register page', () => {
+    spyOn(router, 'navigate');
+    component.goToRegister();
+    expect(router.navigate).toHaveBeenCalledWith(['/register']);
+  });
+
+  it('should navigate to login page', () => {
+    spyOn(router, 'navigate');
+    component.goToLogin();
+    expect(router.navigate).toHaveBeenCalledWith(['/login']);
   });
 });
