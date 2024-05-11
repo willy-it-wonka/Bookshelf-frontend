0a5569efad9094b4c69496c457fcb72391989215
Willy <willy.it.wonka@outlook.com>
2024-05-11 23:10:26 +0200

    Test CreateBookComponent

--- src/app/book/create-book/create-book.component.spec.ts
+++ src/app/book/create-book/create-book.component.spec.ts
@@ -1,14 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateBookComponent } from './create-book.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+import { of, throwError } from 'rxjs';
 
 describe('CreateBookComponent', () => {
   let component: CreateBookComponent;
   let fixture: ComponentFixture<CreateBookComponent>;
+  let bookService: jasmine.SpyObj<BookService>;
 
   beforeEach(async () => {
+    bookService = jasmine.createSpyObj('BookService', ['createBook']);
+
     await TestBed.configureTestingModule({
-      imports: [CreateBookComponent, HttpClientTestingModule],
+      imports: [CreateBookComponent],
+      providers: [{ provide: BookService, useValue: bookService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CreateBookComponent);
@@ -18,5 +24,55 @@ describe('CreateBookComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call saveBook(), resetForm(), showModal() when onSubmit() is called', () => {
+    spyOn(component, 'saveBook');
+    spyOn(component, 'resetForm');
+    spyOn(component, 'showModal');
+
+    component.onSubmit();
+
+    expect(component.saveBook).toHaveBeenCalled();
+    expect(component.resetForm).toHaveBeenCalled();
+    expect(component.showModal).toHaveBeenCalled();
+  });
+
+  it('should reset book properties when resetForm() is called', () => {
+    component.book.title = 'title';
+    component.book.author = 'author';
+    component.book.linkToCover = 'link';
+
+    component.resetForm();
+
+    expect(component.book.title).toBe('');
+    expect(component.book.author).toBe('');
+    expect(component.book.linkToCover).toBe('');
+  });
+
+  it('should call bookService.createBook with the book data from the client when saveBook() is called', () => {
+    const spy = bookService.createBook.and.returnValue(of(new Book()));
+    component.saveBook();
+    expect(spy).toHaveBeenCalledWith(component.book);
+  });
+
+  it('should log response when successful saveBook()', () => {
+    const response = { Book };
+    bookService.createBook.and.returnValue(of(response));
+    spyOn(console, 'log');
+
+    component.saveBook();
+
+    expect(console.log).toHaveBeenCalledWith(response);
+  });
+
+  it('should log error when save fails', () => {
+    const error = new Error('Failed to save book');
+    bookService.createBook.and.returnValue(throwError(() => error));
+    spyOn(console, 'log');
+
+    component.saveBook();
+
+    expect(console.log).toHaveBeenCalledWith(error);
   });
 });
