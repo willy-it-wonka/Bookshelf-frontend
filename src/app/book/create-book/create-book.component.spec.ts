81cb8f9ea83600fd955660c2fa1f526e084621fe
Willy <willy.it.wonka@outlook.com>
2024-07-05 22:20:01 +0200

    Fix CreateBookComponent tests

--- src/app/book/create-book/create-book.component.spec.ts
+++ src/app/book/create-book/create-book.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateBookComponent } from './create-book.component';
 import { BookService } from '../book.service';
 import { Book } from '../book';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 
 describe('CreateBookComponent', () => {
   let component: CreateBookComponent;
@@ -38,18 +38,6 @@ describe('CreateBookComponent', () => {
     expect(component.showModal).toHaveBeenCalled();
   });
 
-  it('should reset book properties when resetForm() is called', () => {
-    component.book.title = 'title';
-    component.book.author = 'author';
-    component.book.linkToCover = 'link';
-
-    component.resetForm();
-
-    expect(component.book.title).toBe('');
-    expect(component.book.author).toBe('');
-    expect(component.book.linkToCover).toBe('');
-  });
-
   it('should call bookService.createBook with the book data from the client when saveBook() is called', () => {
     const spy = bookService.createBook.and.returnValue(of(new Book()));
     component.saveBook();
@@ -66,13 +54,15 @@ describe('CreateBookComponent', () => {
     expect(console.log).toHaveBeenCalledWith(response);
   });
 
-  it('should log error when save fails', () => {
-    const error = new Error('Failed to save book');
-    bookService.createBook.and.returnValue(throwError(() => error));
-    spyOn(console, 'log');
+  it('should reset book properties when resetForm() is called', () => {
+    component.book.title = 'title';
+    component.book.author = 'author';
+    component.book.linkToCover = 'link';
 
-    component.saveBook();
+    component.resetForm();
 
-    expect(console.log).toHaveBeenCalledWith(error);
+    expect(component.book.title).toBe('');
+    expect(component.book.author).toBe('');
+    expect(component.book.linkToCover).toBe('');
   });
 });
