af640fbf8f36b3564d2433f5b03630f2c1f00e7d
Willy <willy.it.wonka@outlook.com>
2024-09-16 23:27:33 +0200

    Generate ChangeDetailsComponent

---
+++ src/app/user/change-details/change-details.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ChangeDetailsComponent } from './change-details.component';
+
+describe('ChangeDetailsComponent', () => {
+  let component: ChangeDetailsComponent;
+  let fixture: ComponentFixture<ChangeDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChangeDetailsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ChangeDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
