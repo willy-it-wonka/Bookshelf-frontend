2a1810541bd8c9a15e6e2db608fc1ca34a99cbbe
Willy <willy.it.wonka@outlook.com>
2024-11-27 21:48:29 +0100

    Fix ForgottenPasswordComponent test file

--- src/app/user/forgotten-password/forgotten-password.component.spec.ts
+++ src/app/user/forgotten-password/forgotten-password.component.spec.ts
@@ -1,16 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ForgottenPasswordComponent } from './forgotten-password.component';
+import { UserService } from '../user.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 describe('ForgottenPasswordComponent', () => {
   let component: ForgottenPasswordComponent;
   let fixture: ComponentFixture<ForgottenPasswordComponent>;
+  let userService: jasmine.SpyObj<UserService>;
+  let dialogRef: jasmine.SpyObj<MatDialogRef<ForgottenPasswordComponent>>;
 
   beforeEach(async () => {
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+    userService = jasmine.createSpyObj('UserService', [
+      'initiateForgottenPasswordReset',
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [ForgottenPasswordComponent]
-    })
-    .compileComponents();
-    
+      imports: [ForgottenPasswordComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRef },
+        { provide: UserService, useValue: userService },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ForgottenPasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
