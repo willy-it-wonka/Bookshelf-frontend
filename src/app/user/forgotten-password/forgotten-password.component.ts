587faaad4021597e02e9dd3feda68e1ae7ae5415
Willy <willy.it.wonka@outlook.com>
2024-11-27 20:27:39 +0100

    Create ForgottenPasswordComponent

---
+++ src/app/user/forgotten-password/forgotten-password.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotten-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './forgotten-password.component.html',
+  styleUrl: './forgotten-password.component.css',
+})
+export class ForgottenPasswordComponent {}
