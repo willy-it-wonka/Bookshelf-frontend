5b4d31a32035d510834ffad17a1590c660cfeafa
Willy <willy.it.wonka@outlook.com>
2024-11-28 23:55:29 +0100

    Implement password reset

--- src/app/user/password-reset/password-reset.component.spec.ts
+++ src/app/user/password-reset/password-reset.component.spec.ts
@@ -1,13 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PasswordResetComponent } from './password-reset.component';
+import { UserService } from '../user.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('PasswordResetComponent', () => {
   let component: PasswordResetComponent;
   let fixture: ComponentFixture<PasswordResetComponent>;
+  let userService: jasmine.SpyObj<UserService>;
 
   beforeEach(async () => {
+    userService = jasmine.createSpyObj('UserService', [
+      'resetForgottenPassword',
+    ]);
+    const activatedRouteMock = {
+      queryParams: of({ token: '123' }),
+    };
+
     await TestBed.configureTestingModule({
       imports: [PasswordResetComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteMock },
+        { provide: UserService, useValue: userService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PasswordResetComponent);
--- src/app/user/password-reset/password-reset.component.ts
+++ src/app/user/password-reset/password-reset.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../user.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-password-reset',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './password-reset.component.html',
   styleUrl: './password-reset.component.css',
 })
-export class PasswordResetComponent {}
+export class PasswordResetComponent {
+  confirmationToken: string = '';
+  newPassword: string = '';
+  hidePassword: boolean = true;
+
+  constructor(
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly userService: UserService,
+    private readonly matSnackBar: MatSnackBar,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.confirmationToken = params['token'];
+    });
+  }
+
+  onSubmit() {
+    this.resetForgottenPassword();
+  }
+
+  resetForgottenPassword(): void {
+    this.userService
+      .resetForgottenPassword(this.confirmationToken, this.newPassword)
+      .subscribe({
+        next: (response) => {
+          this.matSnackBar.open(response, 'Close', { duration: 5000 });
+          this.goToLogin();
+        },
+        error: (error) => {
+          this.matSnackBar.open(error.error, 'Close', { duration: 5000 });
+        },
+      });
+  }
+
+  goToLogin() {
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 5000);
+  }
+}
