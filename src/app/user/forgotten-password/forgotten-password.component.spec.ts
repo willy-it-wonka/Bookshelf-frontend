97e64c6cd842ed770a9fd57360b160de1094ec94
Willy <willy.it.wonka@outlook.com>
2024-11-30 19:51:17 +0100

    Test ForgottenPasswordComponent

--- src/app/user/forgotten-password/forgotten-password.component.spec.ts
+++ src/app/user/forgotten-password/forgotten-password.component.spec.ts
@@ -2,24 +2,31 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ForgottenPasswordComponent } from './forgotten-password.component';
 import { UserService } from '../user.service';
 import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { of, throwError } from 'rxjs';
 
 describe('ForgottenPasswordComponent', () => {
   let component: ForgottenPasswordComponent;
   let fixture: ComponentFixture<ForgottenPasswordComponent>;
   let userService: jasmine.SpyObj<UserService>;
   let dialogRef: jasmine.SpyObj<MatDialogRef<ForgottenPasswordComponent>>;
+  let matSnackBar: jasmine.SpyObj<MatSnackBar>;
+
+  const email = 'test@email.com';
 
   beforeEach(async () => {
     dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
     userService = jasmine.createSpyObj('UserService', [
       'initiateForgottenPasswordReset',
     ]);
+    matSnackBar = jasmine.createSpyObj('MatSnackBar', ['open']);
 
     await TestBed.configureTestingModule({
       imports: [ForgottenPasswordComponent],
       providers: [
         { provide: MatDialogRef, useValue: dialogRef },
         { provide: UserService, useValue: userService },
+        { provide: MatSnackBar, useValue: matSnackBar },
       ],
     }).compileComponents();
 
@@ -30,5 +37,34 @@ describe('ForgottenPasswordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the matDialog on cancel', () => {
+    component.onCancel();
+    expect(dialogRef.close).toHaveBeenCalled();
+  });
+
+  it('should call userService and show success message when password reset initiated successfully', () => {
+    const response = 'Password reset email sent.';
+    userService.initiateForgottenPasswordReset.and.returnValue(of(response));
+    component.email = email;
+
+    component.initiateForgottenPasswordReset();
+
+    expect(userService.initiateForgottenPasswordReset).toHaveBeenCalledWith(email);
+    expect(matSnackBar.open).toHaveBeenCalledWith(response, 'Close', { duration: 5000 });
+    expect(dialogRef.close).toHaveBeenCalled();
+  });
+
+  it('should show error message when password reset initiation fails', () => {
+    const error = { error: 'Error sending email.' };
+    userService.initiateForgottenPasswordReset.and.returnValue(throwError(() => error));
+    component.email = email;
+
+    component.initiateForgottenPasswordReset();
+
+    expect(userService.initiateForgottenPasswordReset).toHaveBeenCalledWith(email);
+    expect(matSnackBar.open).toHaveBeenCalledWith(error.error, 'Close', { duration: 5000 });
+    expect(dialogRef.close).not.toHaveBeenCalled();
   });
 });
